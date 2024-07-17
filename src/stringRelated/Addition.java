7fdbaeca73eb79ae12c68fa016e175a0387938aa
Suresh Nagur <sureshnagur70@gmail.com>
2024-07-17 20:09:21 +0530

    adding new class

---
+++ src/stringRelated/Addition.java
@@ -0,0 +1,15 @@
+package stringRelated;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		int a=5;
+		int b=4;
+		
+		int total=a+b;
+		
+		System.out.println(total);
+
+	}
+
+}
